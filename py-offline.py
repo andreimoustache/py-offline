6f9b459c17b4c90592b130103f55dc11071ae657
Andrei Mustață <andrei.mustata@gmail.com>
2020-05-05 12:27:34 +0300

    Scan body for external resources

--- py-offline.py
+++ py-offline.py
@@ -2,6 +2,8 @@ from os import environ
 from sys import version_info
 from urllib.parse import urlparse
 from pathlib import Path
+import re
+from bs4 import BeautifulSoup
 import requests
 
 print(f'''
@@ -10,13 +12,20 @@ PYOFF_DEPTH={environ["PYOFF_DEPTH"]}
 PYOFF_DESTINATION={environ["PYOFF_DESTINATION"]}
 Running {version_info}''')
 
-_, domain, path, _, _, _ = urlparse(environ["PYOFF_URL"])
+scheme, domain, path, _, _, _ = urlparse(environ["PYOFF_URL"])
 path = "index.html" if path == "/" else path
+site_root = f'{scheme}://{domain}/'
 
 print(f'Domain set to {domain}.')
 print(f'Requesting {path}.')
 response = requests.get(environ["PYOFF_URL"])
 print(f'Got HTTP {response.status_code}, {response.headers["Content-Type"]}.')
+
+document = BeautifulSoup(response.text, "html.parser")
+own_href_resources = document.find_all(href=re.compile(f'^{site_root}'))
+own_src_resources = document.find_all(src=re.compile(f'^{site_root}'))
+print(f'Found {len(own_href_resources)} href resources tags.')
+print(f'Found {len(own_src_resources)} src resources tags.')
 
 write_path = Path(f'{environ["PYOFF_DESTINATION"]}/')
 filename = write_path / path
