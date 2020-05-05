85cc63f9e7fdf5a17a250297f850947ba2803821
Andrei Mustață <andrei.mustata@gmail.com>
2020-05-05 11:42:43 +0300

    Write first path to file

--- py-offline.py
+++ py-offline.py
@@ -1,6 +1,7 @@
 from os import environ
 from sys import version_info
 from urllib.parse import urlparse
+from pathlib import Path
 import requests
 
 print(f'''
@@ -10,8 +11,15 @@ PYOFF_DESTINATION={environ["PYOFF_DESTINATION"]}
 Running {version_info}''')
 
 _, domain, path, _, _, _ = urlparse(environ["PYOFF_URL"])
+path = "index.html" if path == "/" else path
 
 print(f'Domain set to {domain}.')
 print(f'Requesting {path}.')
 response = requests.get(environ["PYOFF_URL"])
 print(f'Got HTTP {response.status_code}, {response.headers["Content-Type"]}.')
+
+write_path = Path(f'{environ["PYOFF_DESTINATION"]}/')
+filename = write_path / path
+print(f'Writing file to {filename}.')
+with open(filename, 'w', encoding=response.encoding) as file:
+  file.write(response.text)
