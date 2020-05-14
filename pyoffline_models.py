008f2b5173a683ed5dda68ba3ade291e31fc6569
Andrei Mustață <andrei.mustata@gmail.com>
2020-05-14 12:30:14 +0300

    Override model str for brevity

--- pyoffline_models.py
+++ pyoffline_models.py
@@ -9,6 +9,13 @@ class Resource:
   body: str = ''
   encoding: str = ''
 
+  def __str__(self):
+    return f'{type(self).__name__}(name={self.name})'
+
+
 @dataclass
 class Document(Resource):
   depth: int = 1
+
+  def __str__(self):
+    return f'{type(self).__name__}(name={self.name}, depth={self.depth})'
