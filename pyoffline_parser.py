c481140c42169ac3b9ee1792ee61524d8f9d743f
Andrei Mustață <andrei.mustata@gmail.com>
2020-05-14 16:00:12 +0300

    Follow relative links

--- pyoffline_parser.py
+++ pyoffline_parser.py
@@ -2,6 +2,7 @@ from logging import getLogger
 from bs4 import BeautifulSoup
 from pyoffline_models import Resource, Document
 from beautifulsoup_extensions import has_href_with_url, has_src_with_url
+from url_extensions import make_url_absolute, make_url_relative
 
 
 class Parser:
@@ -10,15 +11,15 @@ class Parser:
     self.site_root = site_root
 
 
-  def make_link_relative(self, tag, attribute, current_depth=0):
-    url = tag[attribute]
-    relative_url = tag[attribute].replace(self.site_root, "")
+  def process_link(self, tag, attribute, current_depth=0):
+    absolute_url = make_url_absolute(self.site_root, tag[attribute])
+    relative_url = make_url_relative(self.site_root, tag[attribute])
     tag[attribute] = relative_url
 
     if tag.name == "a":
-      resource = Document(url, name=relative_url, depth=current_depth+1)
+      resource = Document(absolute_url, name=relative_url, depth=current_depth+1)
     else:
-      resource = Resource(url, name=relative_url)
+      resource = Resource(absolute_url, name=relative_url)
 
     return resource
 
@@ -28,11 +29,11 @@ class Parser:
 
     href_tags = document.find_all(has_href_with_url(self.site_root))
     self.logger.info(f'Found {len(href_tags)} href resources tags.')
-    resources += [self.make_link_relative(tag, "href",current_depth) for tag in href_tags]
+    resources += [self.process_link(tag, "href",current_depth) for tag in href_tags]
 
     src_tags = document.find_all(has_src_with_url(self.site_root))
     self.logger.info(f'Found {len(src_tags)} src resources tags.')
-    resources += [self.make_link_relative(resource, "src", current_depth) for resource in src_tags]
+    resources += [self.process_link(resource, "src", current_depth) for resource in src_tags]
 
     return resources
 
--- url_extensions.py
+++ url_extensions.py
@@ -5,4 +5,23 @@ def is_own_url(site_root, url):
   """Checks whether `url` is part of the same domain as `site_root`.
   """
   scheme, domain, _, _, _, _ = urlparse(url)
-  return f'{scheme}://{domain}/' in [site_root, ""]
+  if scheme == "" and domain == "":
+    return True
+  else:
+    return f'{scheme}://{domain}/' in [site_root, ""]
+
+
+def make_url_absolute(site_root, url):
+  """Returns an absolute url, with the base `site_root`.
+  """
+  scheme, domain, _, _, _, _ = urlparse(url)
+  if scheme == "" and domain == "":
+    return f'{site_root}/{url}'
+  else:
+    return url
+
+
+def make_url_relative(site_root, url):
+  """Returns a relative url, from the base `site_root`
+  """
+  return url.replace(site_root, "")
