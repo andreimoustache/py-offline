35d8cb6889e29f49bb1331f73c8d8dccbabbb168
Andrei Mustață <andrei.mustata@gmail.com>
2020-05-15 09:39:31 +0300

    Remove redundant layer

--- py-offline.py
+++ py-offline.py
@@ -13,14 +13,21 @@ from queue_processors import processor, fork_processor
 from url_extensions import make_url_relative
 
 
-def process_site(config:Config,
-                  urls: Queue,
-                  resources: Queue,
-                  files: Queue,
-                  downloader:Downloader,
-                  parser: Parser,
-                  writer: Writer):
+def main():
+  try:
+    config = Config(environ)
+  except ConfigException as ex:
+    logger.fatal(ex.args)
+    return exit(1)
 
+  logger.info("Starting py-offline")
+  logger.info(config)
+
+  downloader = Downloader(requests)
+  parser = Parser(config.site_root)
+  writer = Writer(config.write_path)
+
+  urls, resources, files = Queue(), Queue(), Queue()
   first_path = make_url_relative(config.site_root, config.site_url)
   first_document = Document(config.site_url, name=first_path, depth=0)
   urls.put(first_document)
@@ -38,24 +45,6 @@ def process_site(config:Config,
   files.join()
 
 
-def main(logger: logging.Logger):
-  try:
-    config = Config(environ)
-  except ConfigException as ex:
-    logger.fatal(ex.args)
-    return exit(1)
-
-  logger.info("Starting py-offline")
-  logger.info(config)
-
-  downloader = Downloader(requests)
-  parser = Parser(config.site_root)
-  writer = Writer(config.write_path)
-
-  urls, resources, files = Queue(), Queue(), Queue()
-  process_site(config, urls, resources, files, downloader, parser, writer)
-
-
 if __name__ == "__main__":
   log_level = environ.get("LOGLEVEL", "INFO")
   log_format = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
@@ -63,4 +52,4 @@ if __name__ == "__main__":
   logging.basicConfig(level=log_level, format=log_format)
   logger = logging.getLogger(__name__)
 
-  main(logger)
+  main()
