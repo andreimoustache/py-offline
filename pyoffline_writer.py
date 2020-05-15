9c8768e0e4415f02fe6513eb97720141231ffa56
Andrei Mustață <andrei.mustata@gmail.com>
2020-05-15 09:29:47 +0300

    Drop leading slash from filename

--- pyoffline_writer.py
+++ pyoffline_writer.py
@@ -13,7 +13,8 @@ class Writer:
 
 
   def write(self, resource: Resource):
-    filename = PurePath(self.path / resource.name)
+    name = resource.name[1:]
+    filename = PurePath(self.path / name)
     self.logger.info(f'Writing file to {filename}.')
     makedirs(filename.parent, exist_ok=True)
     with open(filename, 'w', encoding="utf-8") as file:
